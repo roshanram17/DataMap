3c9fc59438eb8b46fdd7b309cac79ec4becd3c48
roshanram17 <51178227+roshanram17@users.noreply.github.com>
2022-01-17 12:01:24 -0500

    add code

--- dataMao.py
+++ dataMao.py
@@ -1,9 +1,15 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
+import streamlit as st
+import altair as alt
 
-df = pd.DataFrame(
-     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-     columns=['lat', 'lon'])
 
-st.map(df)
+st.set_page_config(layout="wide")
+
+dataFrame = pd.DataFrame(np.random.randn(2000, 2) / [10, 10] + [43.65, -79.34], columns=['lat', 'lon'])
+
+if st.checkbox('Show dataframe'):
+     st.map(dataFrame)
+print(dataFrame)
+
+st.map(dataFrame)
